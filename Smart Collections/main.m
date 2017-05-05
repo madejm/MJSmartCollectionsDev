abee395de09f823791164c95a2befe6119fcd04c
Mateusz Madej <madejmt@gmail.com>
2017-05-05 19:57:08 +0200

    Initial Commit

---
+++ Smart Collections/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Smart Collections
+//
+//  Created by Mejdej on 05/05/17.
+//  Copyright (c) 2017 Mejdej. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
